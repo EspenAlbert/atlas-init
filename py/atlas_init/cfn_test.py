a5f3abc619e1d311c7fc2c20dfec42b514f9fc68
EspenAlbert <espen.albert@mongodb.com>
2024-05-03 09:31:44 +0100

    fix: ensure type_name is exact match

--- py/atlas_init/cfn_test.py
+++ py/atlas_init/cfn_test.py
@@ -5,5 +5,6 @@ from atlas_init.cfn import get_last_cfn_type
 
 def test_get_last_cfn_type():
     aws_profile = os.environ["AWS_PROFILE"]
-    t = get_last_cfn_type("MongoDB::Atlas::Project", region="us-east-1", is_third_party=True)
+    t = get_last_cfn_type("MongoDB::Atlas::Cluster", region="us-east-1", is_third_party=True)
+    print(t)
     assert t
