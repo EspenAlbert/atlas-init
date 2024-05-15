e1e557fcac11cc86e457688475005654983cc1fa
EspenAlbert <espen.albert@mongodb.com>
2024-05-15 20:57:43 +0100

    feat: more strict running_in_repo check

--- py/atlas_init/__init__.py
+++ py/atlas_init/__init__.py
@@ -4,6 +4,9 @@ VERSION = "0.0.2"
 
 
 def running_in_repo() -> bool:
-    git_directory = Path(__file__).parent.parent.parent / ".git"
+    py_directory = Path(__file__).parent.parent
+    if py_directory.name != "py":
+        return False
+    git_directory = py_directory.parent / ".git"
     fetch_head = git_directory / "FETCH_HEAD"
     return git_directory.exists() and fetch_head.exists() and "atlas-init" in fetch_head.read_text()
