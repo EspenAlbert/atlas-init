3e3dc5a4b62aca153cd439c8c566e08cf82ee691
EspenAlbert <espen.albert@mongodb.com>
2024-05-15 08:36:54 +0100

    ci: remove dependency installing to the pyproject.toml

--- py/file_copy.py
+++ py/file_copy.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Any
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from zero_3rdparty.file_utils import clean_dir, copy, iter_paths_and_relative
 
 REL_PATH_FILES = [
     "atlas_init.yaml",
@@ -17,8 +18,6 @@ logger = logging.getLogger(__name__)
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        from zero_3rdparty.file_utils import copy, iter_paths_and_relative
-
         for rel_path in REL_PATH_FILES:
             copy(REPO_PATH / rel_path, ATLAS_INIT_PATH / rel_path)
         for tf_path, tf_rel_path in iter_paths_and_relative(REPO_PATH / "tf", "*.tf", only_files=True):
@@ -26,13 +25,7 @@ class CustomBuildHook(BuildHookInterface):
             copy(tf_path, dest_path)
         return super().initialize(version, build_data)
 
-    def dependencies(self) -> list[str]:
-        extras = ["zero-3rdparty==0.0.30"]
-        return super().dependencies() + extras
-
     def clean(self, versions: list[str]) -> None:
-        from zero_3rdparty.file_utils import clean_dir
-
         for rel_path in REL_PATH_FILES:
             logger.warning(f"will remove again: {rel_path}")
             dest_path = ATLAS_INIT_PATH / rel_path
