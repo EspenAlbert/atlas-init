83e620232d66d6e8d1d3df2a749edee4ac2e4da8
EspenAlbert <espen.albert@mongodb.com>
2024-06-12 10:56:34 +0200

    pre-commit script fixes

--- py/pre-commit.sh
+++ py/pre-commit.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 repo_root=$(git rev-parse --show-toplevel)
-export PYTHONPATH=$repo_root/py
+echo "running from $repo_root"
+PYTHONPATH=$repo_root/py
+echo "PYTHONPATH=$PYTHONPATH"
 cd $PYTHONPATH
 hatch fmt
+echo "Files are formatted ✅"
 hatch run default:fmt_config
 echo "pre-commit-ok!"
