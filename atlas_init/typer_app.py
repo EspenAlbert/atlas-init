e25a3525436f6dbb36d909805d47a9bf65d96377
Espen Albert <espen.albert@mongodb.com>
2025-05-22 10:44:38 +0100

    feat: add skip_ambiguous_check parameter to allow the file to be updated by terraform apply without failing afterwards

--- atlas_init/typer_app.py
+++ atlas_init/typer_app.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 def sync_on_done(return_value, s3_profile_bucket: str = "", use_clipboard: str = "", **kwargs):
     logger.info(f"sync_on_done return_value={return_value} and {kwargs}")
-    settings = init_settings()
+    settings = init_settings(skip_ambiguous_check=True)
     if s3_profile_bucket:
         logger.info(f"using s3 bucket for profile sync: {s3_profile_bucket}")
         upload_to_s3(settings.profile_dir, s3_profile_bucket)
