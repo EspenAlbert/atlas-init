ab50931ea75802e8f0b94b052c3174492d8b836b
Espen Albert <espen.albert@mongodb.com>
2025-07-17 19:22:46 +0100

    feat: add examples for various MongoDB Atlas cluster configurations and update plan checks

--- test_atlas_init/test_tf_ext/livedata/modules/cluster_skip_python/examples/05_multi-region-single-geo/main.tf
+++ test_atlas_init/test_tf_ext/livedata/modules/cluster_skip_python/examples/05_multi-region-single-geo/main.tf
@@ -1,15 +1,15 @@
 module "cluster" {
   source                 = "../.."
   project_id             = var.project_id
-  name                   = "multi-cloud"
+  name                   = "multi-region-single-geo-no-sharding"
   cluster_type           = "REPLICASET"
   mongo_db_major_version = "8.0"
   replication_specs = [
     {
       region_configs = [
         {
-          provider_name = "AZURE"
-          region_name   = "US_WEST_2"
+          provider_name = "AWS"
+          region_name   = "US_EAST_1"
           priority      = 7
           electable_specs = {
             instance_size = "M30"
